da4f5892810582eb1c143b47e89c5248d7519a0f
Eze Capretta <caprettaeze@gmail.com>
2024-05-09 13:14:34 -0300

    esquema del proyecto

---
+++ src/components/Pricing.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Pricing = () => {
+  return (
+    <div>
+      <p>precios</p>
+    </div>
+  );
+};
+
+export default Pricing;
---
+++ src/components/Testimonials.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Testimonials = () => {
+  return (
+    <div>
+      <p>testimonios</p>
+    </div>
+  );
+};
+
+export default Testimonials;
