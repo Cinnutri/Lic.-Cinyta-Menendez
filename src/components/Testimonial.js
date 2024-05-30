dc7dfd6233ddae11d4359be0b90ff5ae38e1926d
Eze Capretta <caprettaeze@gmail.com>
2024-05-30 20:06:36 -0300

    testimonial

--- src/components/Testimonial.js
+++ src/components/Testimonial.js
@@ -1,7 +1,7 @@
-import React, { useState } from "react";
+import React from "react";
 import Image from "next/image";
 import testimonialMatias from "@../assets/testimonial/testimonial-Matias.jpg"
-import tesimonial02 from "@../assets/testimonial/testimonial-02.jpg";
+import tesimonialFabricio from "@../assets/testimonial/testimonial-Fabricio.jpg";
 import tesimonial03 from "@../assets/testimonial/testimonial-03.jpg";
 
 const testimonials = [
@@ -11,9 +11,9 @@ const testimonials = [
     image: testimonialMatias,
   },
   {
-    text: "La primera vez llegué a ella con el objetivo de incrementar la performance en mi entrenamiento de Crossfit, y la diferencia fue muy notoria, tanto en rendimiento, como en los cambios en mi cuerpo, acompañados de pérdida de peso con ganancia de masa muscular. Años después acudí a ella con el objetivo de ponerme en forma luego de un largo periodo de inactividad. A razón de pocos meses logramos un notorio aumento de masa muscular con pérdida de grasa y reducción de peso. También tuve la oportunidad de realizar un curso de Nutrición Deportiva, donde ella fue la docente y pude corroborar tanto sus grandes conocimientos, como su vocación para transmitirlos.",
-    name: "Alfredo Arias",
-    image: tesimonial02,
+    text: "Mi experiencia con cintya a sido muy buena, es mi nutriciónista hace 4 años si mal no recuerdo, en las consultas siempre me he sentido muy cómodo y siempre a evacuado mis dudas sobre la alimentación. Yo acudí a ella porque quería bajar de peso y fue la mejor decisión que tomé, baje de peso rápidamente siguiendo el plan de alimentación que me pacto. Luego de que logramos bajar el peso que tenia que bajar, me cambio el plan para aumentar la masa muscular y la aumente mucho, ya que mi objetivo era competir en fisicoculturismo y así pude lograrlo gracias a los planes alimenticios qué me puso. Le estoy muy agradecido por la ayuda y el apoyo que me ha dado y me sigue brindando.",
+    name: "Fabricio",
+    image: tesimonialFabricio,
   },
   {
     text: "Acudimos a Cintya hace 3 años ya que necesitábamos orientación en nuestra alimentación. Yo concurrí con problemas gástricos y renales y mi esposa para un control y recomendación en su alimentación. Gracias a las indicaciones y orientación de Cintya, superé los problemas gástricos y renales con el paso del tiempo y pude alimentarme correctamente. Mi esposa consiguió la ayuda que buscaba en su orden de alimentación y lo más importante es que toda esa ayuda y guía es con método natural y totalmente saludable.",
@@ -29,9 +29,9 @@ const testimonials = [
 
 const Testimonial = () => {
   return (
-    <div className="bg-green-500 p-4 flex flex-col items-center">
+    <div className="p-4 flex flex-col items-center  text-[#333] font-[sans-serif] shadow-custom">
       <div className="my-4">
-        <p className="text-white text-4xl font-bold text-center">
+        <p className="text-4xl font-bold text-center">
           Testimonios de nuestros clientes
         </p>
       </div>
@@ -40,14 +40,14 @@ const Testimonial = () => {
           <div
             key={index}
             className={`
-            flex gap-4 p-4 bg-white rounded-md
-            medium:flex-row medium:items-center medium:justify-between
+            flex gap-4 p-4 bg-green-500 rounded-md  text-[#333] font-[sans-serif] shadow-custom
+            medium:flex-row medium:items-center medium:justify-between medium:h-[400px]
             xSmall:flex-col xSmall:items-center xSmall:justify-between
             `}
           >
             {index % 2 === 0 ? (
               <>
-                <div className="flex flex-col items-center text-center medium:w-full">
+                <div className="flex flex-col items-center text-center medium:w-8/12 meium:h-full">
                   <Image
                     src={testimonial.image}
                     alt={testimonial.name}
@@ -56,18 +56,18 @@ const Testimonial = () => {
                     className=""
                   />
                 </div>
-                <div className="flex flex-col items-center">
-                  <p className="text-gray-700 font-bold text-center">{testimonial.name}</p>
-                  <p className="text-gray-700 medium:w-8/12 text-center">{testimonial.text}</p>
+                <div className="flex flex-col items-center meium:h-full medium:w-4/6">
+                  <p className="text-white font-bold text-center">{testimonial.name}</p>
+                  <p className="text-white text-center">{testimonial.text}</p>
                 </div>
               </>
             ) : (
               <>
-                <div className="flex flex-col items-center">
-                  <p className="text-gray-700 font-bold text-center">{testimonial.name}</p>
-                  <p className="text-gray-700 medium:w-8/12 text-center">{testimonial.text}</p>
+                <div className="flex flex-col items-center meium:h-full medium:w-4/6">
+                  <p className="text-white font-bold text-center">{testimonial.name}</p>
+                  <p className="text-white text-center">{testimonial.text}</p>
                 </div>
-                <div className="flex flex-col items-center text-center medium:w-full">
+                <div className="flex flex-col items-center text-center medium:w-8/12 meium:h-full">
                   <Image
                     src={testimonial.image}
                     alt={testimonial.name}
